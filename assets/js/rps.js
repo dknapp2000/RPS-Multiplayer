47754961c020b2bbc9f48138d4f07283f4dbd5cb
dknapp2000 <don@dknapp.com>
2017-01-23 13:24:37 -0500

    Work in process. Associating actions with remote data changes.

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -27,6 +27,7 @@ var rps = {
     ptrLoggedIn: "",
     ptrResetButton: "",
     ptrGameInProgress: "",
+    ptrPlayerName: ["", ""],
 
     comTemplate: "",
 
@@ -40,6 +41,7 @@ var rps = {
 
     startup: function() {
         console.log( "Function: startup");
+        // Here is the one-time grab of all the document elements that we'll be using.
         ptrName = document.getElementById('name');
         ptrPromise = document.getElementById('promise');
         ptrLoginButton = document.getElementById('login-button');
@@ -48,6 +50,8 @@ var rps = {
         ptrLoggedIn = document.querySelectorAll(".logged-in");
         ptrResetButton = document.getElementById( "reset-data" );
         ptrGameInProgress = document.getElementById( "game-in-progress" );
+        rps.ptrPlayerName[0] = document.querySelector( ".zone1 .player-name" );
+        rps.ptrPlayerName[1] = document.querySelector( ".zone3 .player-name" );
 
         firebase.initializeApp(rps.fireBaseConfig);
         rps.db = firebase.database();
@@ -57,8 +61,11 @@ var rps = {
         ptrLoginButton.addEventListener( "click", rps.nameEntered );
         ptrResetButton.addEventListener( "click", rps.resetData );
 
-        // Grab a copy of the empty com object so we can reset the firebase data if requested.
+        /* Grab a copy of the empty com object so we can reset the firebase
+         * data if requested.  (There must be a better way to do this)
+         */
         comTemplate = JSON.parse( JSON.stringify( rps.com ) );
+
         /* The data names below will be "watched", any changes to these elements
          * in the firebase database will be automatically reflected in the
          * json object named rps.com.data.{name-of-the-watched-value}
@@ -73,6 +80,12 @@ var rps = {
         rps.watch( "playerCount" );
         rps.watch( "state" );
         rps.watch( "users" );
+        //rps.actOn( "players/0/name", rps.setName1() );
+        //rps.actOn( "players/1/name", rps.setName2() );
+        rps.db.ref("/data/players/0/name").on( "value", function(snap){
+          console.log( "players/0/name has changed", snap.val() );
+          rps.ptrPlayerName[0].innerHTML = snap.val() ;
+        } );
     },
     /* This is an important "setter" that stores data in the "/data" already
      * in the firebase database.  This can be used in conjunction with the
@@ -97,9 +110,29 @@ var rps = {
             rps.setGetLog( "Get:", key, snap.val());
         });
     },
-
+    /* This logs set/get actions to the console.  It's a separate
+     * method so that it can be turned off in a single place when
+     * I no longer need it.
+     */
     setGetLog: function( action, key, value ) {
         console.log( "SG:", action, key, value );
+    },
+    /* OK, a thing of beauty is a joy forever. . .
+     * actOn: Using the same method as the watch method, we'll "watch" for changes on
+     * individual data elements and act (callback) on any change.
+     */
+    actOn: function( key, callback ) {
+      rps.db.ref("/data/" + key).on( "value", callback( snap ) );
+    },
+
+    setName1: function( snap ) {
+        console.log( "setName1: ", snap.val() );
+        rps.ptrPlayerName[0].innerHTML = snap.val();
+    },
+
+    setName1: function( snap ) {
+        console.log( "setName1: ", snap.val() );
+        rps.ptrPlayerName[1].innerHTML = snap.val();
     },
 
     updateState: function( snap ) {
@@ -148,6 +181,7 @@ var rps = {
 
         rps.me.name = name;
         rps.me.id = rps.com.data.playerCount;
+
         rps.set("playerCount", ++rps.com.data.playerCount );
         rps.set("players/" + rps.me.id, rps.me );
 
